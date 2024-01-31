0283b8cb70a23828ca5b72d22dd3ff6d38db6604
ddonydev <ddonydev@gmail.com>
2024-01-31 09:13:13 +0900

    initial commit

---
+++ src/test/java/ddonyblog/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ddonyblog.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
